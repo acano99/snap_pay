e6f5e082491273b9445f82c2b8677747b2cb7e42
acano <abrahan.jcano99@gmail.com>
2025-03-27 16:03:16 -0400

    feat: implementacion de los metodos para el tema

--- packages/snap_pay_local_storage_api/lib/src/snap_pay_local_storage_api.dart
+++ packages/snap_pay_local_storage_api/lib/src/snap_pay_local_storage_api.dart
@@ -1,6 +1,12 @@
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:snap_pay_api/snap_pay_api.dart';
 
 class SnapPayLocalStorageApi extends SnapPayApi {
+  final SharedPreferences _sharedPreferences;
+
+  SnapPayLocalStorageApi({required SharedPreferences sharedPreferences})
+      : _sharedPreferences = sharedPreferences;
+
   @override
   Future<void> deletePaymentMethod(PaymentMethod paymentMethod) {
     // TODO: implement deletePaymentMethod
@@ -24,4 +30,12 @@ class SnapPayLocalStorageApi extends SnapPayApi {
     // TODO: implement updatePaymentMethod
     throw UnimplementedError();
   }
+
+  @override
+  Future<bool> isDarkTheme() async =>
+      _sharedPreferences.getBool('isDarkTheme') ?? false;
+
+  @override
+  Future<void> setDarkTheme(bool isDarkTheme) async =>
+      _sharedPreferences.setBool('isDarkTheme', isDarkTheme);
 }
