7f247b1baf40fab87f76aeaee2dafa03141cf0b5
acano <abrahan.jcano99@gmail.com>
2025-03-27 17:20:09 -0400

    update: actualizacion de la ui

--- lib/src/start_screen.dart
+++ lib/src/start_screen.dart
@@ -8,6 +8,7 @@ class NavigationBarScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: const Text('Snap Pay')),
       body: navigationShell,
       bottomNavigationBar: NavigationBar(
         selectedIndex: navigationShell.currentIndex,
