206e5b0d45ee9865a0b0714982f4331698190e81
acano <abrahan.jcano99@gmail.com>
2025-03-27 08:04:35 -0400

    feat: ui para la home de los paymentmethods

---
+++ lib/src/payment_methods/ui/payment_method_home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PaymentMethodHomeScreen extends StatelessWidget {
+  const PaymentMethodHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
