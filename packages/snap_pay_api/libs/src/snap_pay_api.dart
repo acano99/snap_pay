7cd69745417ebed4201cd02cfd8390f268668711
acano <abrahan.jcano99@gmail.com>
2025-03-27 04:08:23 -0400

    add: api para los metodos de pago

--- packages/snap_pay_api/libs/src/snap_pay_api.dart
+++ packages/snap_pay_api/libs/src/snap_pay_api.dart
@@ -1,1 +1,11 @@
-class SnapPayApi {}
+import 'models/payment_method.dart';
+
+abstract class SnapPayApi {
+  Future<List<PaymentMethod>> getPaymentMethods();
+
+  Future<void> savePaymentMethod(PaymentMethod paymentMethod);
+
+  Future<void> deletePaymentMethod(PaymentMethod paymentMethod);
+
+  Future<void> updatePaymentMethod(PaymentMethod paymentMethod);
+}
