dd02468658076bd1187d72bcaf5d8fa415fe2cec
acano <abrahan.jcano99@gmail.com>
2025-03-27 04:20:23 -0400

    feat-fix: implementacion de la interfaz SnapPayApi y arreglo de la ruta

---
+++ packages/snap_pay_local_storage_api/lib/src/snap_pay_local_storage_api.dart
@@ -0,0 +1,27 @@
+import 'package:snap_pay_api/snap_pay_api.dart';
+
+class SnapPayLocalStorageApi extends SnapPayApi {
+  @override
+  Future<void> deletePaymentMethod(PaymentMethod paymentMethod) {
+    // TODO: implement deletePaymentMethod
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<PaymentMethod>> getPaymentMethods() {
+    // TODO: implement getPaymentMethods
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> savePaymentMethod(PaymentMethod paymentMethod) {
+    // TODO: implement savePaymentMethod
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> updatePaymentMethod(PaymentMethod paymentMethod) {
+    // TODO: implement updatePaymentMethod
+    throw UnimplementedError();
+  }
+}
---
+++ packages/snap_pay_repository/lib/snap_pay_repository.dart
@@ -0,0 +1,1 @@
+export 'src/snap_pay_repository.dart';
